ba28e213daa4390350749d1d929676d67b8b27a8
ohnomachadom1205 <OhnoMachadoM1205@sduhsd.lan>
2016-09-06 08:46:12 -0700

    Added Files

---
+++ APLesson_01/Profirst.java
@@ -0,0 +1,14 @@
+public class Profirst
+{
+	public static void main (String[]args)
+	{
+
+		System.out.println("  q-p");
+		System.out.println("  [   ]");
+		System.out.println("^     ^");
+		System.out.println("  -----");
+		System.out.println("    )");
+		System.out.println("    (");
+		
+	}
+}
---
+++ APLesson_01/ProjectOne.java
@@ -0,0 +1,12 @@
+public class ProjectOne
+{
+	public static void main (String[]args)
+	{
+		System.out.println("^    ^");
+		System.out.println("@..@");
+		System.out.println("  ----");
+		System.out.println("<__>");
+		System.out.println("^----^");
+		
+	}
+}
