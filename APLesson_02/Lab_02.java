04231f37f6ab9ab1f2b85a42f1c3fd1ceba8e700
ohnomachadom1205 <mohnomachado@gmail.com>
2016-10-04 08:59:38 -0700

    changed

--- APLesson_02/Lab_02.java
+++ APLesson_02/Lab_02.java
@@ -1,19 +1,28 @@
+
 public class Lab_02
 {
 	public static void main(String[]args)
 	{
-		int x = 4;
-		int y = 4;
-		String Name = "Marcelo";
+		//prints product of 2 numbers
+		int num1 = 2;
+		int num2 = 4;
+		System.out.println(num1+" multiplied by "+num2+" is "+num1*num2);
+		
+		System.out.println();
+		
+		//prints address label
+		String name = "Marcelo Ohno-Machado";
 		String address = "4044 Via Cangrejo";
-		String city = "San Diego";
+		String city = "San Diego, CA";
 		String zip = "92130";
-		int length = 9;
-		int width = 9;
-		int height = 9;
-		System.out.println("The surface area of a cube with edge length 9 is " + length*width*6);
-		System.out.println( Name + " lives in " + address + " , " + city + " , " + zip);
-		System.out.println(" 4 times 4 is " + x * y);
-	
+		System.out.println(name+"\n"+address+"\n"+city+", "+zip);
+		
+		System.out.println();
+		
+		//prints calculated surface area
+		int length = 3;
+		int height = 4;
+		int width = 5;
+		System.out.println("The surface area of your rectangle is "+length*width*height);
 	}
 }
