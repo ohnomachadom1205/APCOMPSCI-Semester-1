8935cccb332cc84511ab2f3817ccf1a15271a100
ohnomachadom1205 <mohnomachado@gmail.com>
2016-11-01 07:55:40 -0700

    fixed lab

--- APLesson_04/Lab4_1ex3.java
+++ APLesson_04/Lab4_1ex3.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Lab4_1ex3
 {
-	public static void main(String[]args)
+	public double payment(double r,double p,double n,double t)
 	{
-		Lab4_1ex3 totalCost = new Lab4_1ex3();
-		double r = 0.09;
-		double P = 1000;
-		double n = 120;
-		double t = 68;
-		double total = totalCost.CalcComInt(r, P, n, t);
-		System.out.printf("Your total monthly payment is $%5.2f", total); 
+		double cost = p*Math.pow(1+r/n,n*t);
+		double payment = cost/(t*12);
+		return payment;
 	}
 	
-	public double CalcComInt(double r, double P, double n, double t)
+	public static void main(String[]args)
 	{
-		return (P* (Math.pow(1 + r/n, n*t)))/(t*12);
+		
+		Scanner sc = new Scanner(System.in);
+		Lab4_1ex3 ci = new Lab4_1ex3();
+		
+		System.out.print("Enter principal: ");
+		double princpl = sc.nextDouble();
+		System.out.print("Enter interest rate: ");
+		double rate = sc.nextDouble();
+		System.out.print("Enter number of times compounded per year: ");
+		double number = sc.nextDouble();
+		System.out.print("Enter life of loan: ");
+		double time = sc.nextDouble();
+		
+		double payment = ci.payment(rate,princpl,number,time);
+		
+		System.out.printf("Your total monthly payment is $%.2f",payment);
 	}
 }
--- APLesson_04/Lab_04ex1.java
+++ APLesson_04/Lab_04ex1.java
@@ -1,36 +1,47 @@
+import java.util.Scanner;
+
 public class Lab_04ex1
 {
+	public void format(String item, double price)
+	{
+		
+		System.out.printf("*%17s ........%10.2f\n",item,price);
+	}
 	public static void main(String[]args)
 	{
-		Lab_04ex1 form = new Lab_04ex1();
 		
-		System.out.printf("%6s   %10.2f", "Cheese Sandwich.....", 4.50);
-		String word1 = "French Fries.....";
-		double number1 = 2.88;
+		Scanner sc = new Scanner(System.in);
+		Lab_04ex1 r = new Lab_04ex1();
 		
-		form.format(word1, number1);
+		System.out.println("Please enter item 1:");
+		String i1 = sc.nextLine();
+		System.out.println("Please enter the price:");
+		double p1 = sc.nextDouble();
+		sc.nextLine();
+		System.out.println("Please enter item 2:");
+		String i2 = sc.nextLine();
+		System.out.println("Please enter the price:");
+		double p2 = sc.nextDouble();
+		sc.nextLine();
+		System.out.println("Please enter item 3:");
+		String i3 = sc.nextLine();
+		System.out.println("Please enter the price:");
+		double p3 = sc.nextDouble();
+		sc.nextLine();
 		
-		String word2 = "Soda.....";
-		double number2 = 1.63;
-		form.format(word2, number2);
+		double sub = p1+p2+p3;
+		double tax = sub*0.07;
+		double tot = sub+tax;
 		
-		String word3 = "Subtotal.....";
-		double number3 = 9.01;
-		form.format(word3, number3);
-		
-		String word4 = "Tax.....";
-		double number4 = 0.69;
-		form.format(word4, number4);
-		
-		String word5 = "Total.....";
-		double number5 = 9.64;
-		form.format(word5, number5);
-		
-		
-	}
-	
-	public void format(String word, double number)
-	{
-		System.out.printf("\n%10s  %10.2f", word, number);
+		System.out.println("<<<<<<<<<<<<<<<__Receipt__>>>>>>>>>>>>>>>>\n\n");
+		r.format(i1,p1);
+		r.format(i2,p2);
+		r.format(i3,p3);
+		System.out.println("\n");
+		r.format("Subtotal:",sub);
+		r.format("Tax:",tax);
+		r.format("Total:",tot);
+		System.out.println("__________________________________________");
+		System.out.println(" * Thank you for your support *");
 	}
 }
